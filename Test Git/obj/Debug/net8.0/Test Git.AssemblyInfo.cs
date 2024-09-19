8da4b48db482abb1a02acf9e67d0f2814e6050c2
Pobwjj <hotpanda007@gmail.com>
2024-09-19 14:07:05 +0700

    Them class moi

--- Test Git/obj/Debug/net8.0/Test Git.AssemblyInfo.cs
+++ Test Git/obj/Debug/net8.0/Test Git.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Test Git")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a49299fb526bcef65e206159be6591282edcf0fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("Test Git")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Test Git")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
